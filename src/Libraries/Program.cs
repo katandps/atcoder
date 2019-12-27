3967253377243c66a353befcc5425fc712a30974
katand <katandps@gmail.com>
2019-12-27 18:09:41 +0900

    入力ライブラリの改造

--- src/Libraries/Program.cs
+++ src/Libraries/Program.cs
@@ -1,16 +1,140 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Libraries
 {
-    internal class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            for (int i = 0; i < 50; i++)
-            {
-                Console.WriteLine(i);
-                Console.WriteLine((int) (Modular.Fac(i) / Modular.Fac(i / 2) / Modular.Fac(i - i / 2)));
-            }
+            Solver solver = new Solver();
+            solver.Solve();
         }
     }
-}
+
+    class Solver
+    {
+        public Solver()
+        {
+            Input input = new Input();
+        }
+
+        public void Solve()
+        {
+            Console.WriteLine(0);
+        }
+    }
+
+    class Input
+    {
+        /// <summary>
+        /// 1行の入力を取得する
+        /// </summary>
+        /// <returns>文字列</returns>
+        public string String()
+        {
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 複数行の入力を取得
+        /// </summary>
+        /// <returns>文字列の配列</returns>
+        public string[] ArrayString(int rowNumber)
+        {
+            string[] s = new string[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                s[i] = String();
+            }
+
+            return s;
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>int型の値</returns>
+        public int Int()
+        {
+            return int.Parse(String());
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>int型の配列</returns>
+        public int[] ArrayInt()
+        {
+            return Split().Select(int.Parse).ToArray();
+        }
+
+        /// <summary>
+        /// 複数行の入力を取得
+        /// </summary>
+        /// <param name="rowNumber">行数</param>
+        /// <returns>int型の配列</returns>
+        public int[] ArrayInt(int rowNumber)
+        {
+            int[] ints = new int[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                ints[i] = Int();
+            }
+
+            return ints;
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>long型の値</returns>
+        public long Long()
+        {
+            return long.Parse(String());
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>long型の配列</returns>
+        public long[] ArrayLong()
+        {
+            return Split().Select(long.Parse).ToArray();
+        }
+
+        public long[] ArrayLong(int rowNumber)
+        {
+            long[] longs = new long[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                longs[i] = Long();
+            }
+
+            return longs;
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>double型の値</returns>
+        public double Double()
+        {
+            return double.Parse(String());
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>double型の配列</returns>
+        public double[] ArrayDouble()
+        {
+            return Split().Select(double.Parse).ToArray();
+        }
+
+        private IEnumerable<string> Split()
+        {
+            return String().Split(' ');
+        }
+    } }
