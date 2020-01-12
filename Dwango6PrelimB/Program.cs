2dab0b1da504b996ee7f4464dbeccbaf32822e66
katand <katandps@gmail.com>
2020-01-12 20:49:37 +0900

    Dwango6

---
+++ Dwango6PrelimB/Program.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+// namespaceの値をコンテスト名にして運用
+namespace Dwango6PrelimB
+{
+    class Input
+    {
+        /// <summary>
+        /// 1行の入力を取得する
+        /// </summary>
+        /// <returns>文字列</returns>
+        public string String()
+        {
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 複数行の入力を取得
+        /// </summary>
+        /// <returns>文字列の配列</returns>
+        public string[] ArrayString(int rowNumber)
+        {
+            string[] s = new string[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                s[i] = String();
+            }
+
+            return s;
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>int型の値</returns>
+        public int Int()
+        {
+            return int.Parse(String());
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>int型の配列</returns>
+        public int[] ArrayInt()
+        {
+            return Split().Select(int.Parse).ToArray();
+        }
+
+        /// <summary>
+        /// 複数行の入力を取得
+        /// </summary>
+        /// <param name="rowNumber">行数</param>
+        /// <returns>int型の配列</returns>
+        public int[] ArrayInt(int rowNumber)
+        {
+            int[] ints = new int[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                ints[i] = Int();
+            }
+
+            return ints;
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>long型の値</returns>
+        public long Long()
+        {
+            return long.Parse(String());
+        }
+
+        /// <summary>
+        /// 2つの整数が1行に書かれている入力を、2つのlongで受け取る
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        public void Longs(ref long A, ref long B)
+        {
+            var longs = ArrayLong();
+            A = longs[0];
+            B = longs[1];
+        }
+
+        /// <summary>
+        /// 3つの整数が1行に書かれている入力を、3つのlongで受け取る
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="C"></param>
+        public void Longs(ref long A, ref long B, ref long C)
+        {
+            var longs = ArrayLong();
+            A = longs[0];
+            B = longs[1];
+            C = longs[2];
+        }
+
+        /// <summary>
+        /// 4つの整数が1行に書かれている入力を、4つのlongで受け取る
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="C"></param>
+        /// <param name="D"></param>
+        public void Longs(ref long A, ref long B, ref long C, ref long D)
+        {
+            var longs = ArrayLong();
+            A = longs[0];
+            B = longs[1];
+            C = longs[2];
+            D = longs[3];
+        }
+
+        /// <summary>
+        /// 2つの整数が複数行に書かれている入力を、2つのlong[]で受け取る
+        /// </summary>
+        /// <param name="rowNumber"></param>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        public void LongsArray(long rowNumber, ref long[] A, ref long[] B)
+        {
+            A = new long[rowNumber];
+            B = new long[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                var l = ArrayLong();
+                A[i] = l[0];
+                B[i] = l[1];
+            }
+        }
+
+        /// <summary>
+        /// 3つの整数が複数行に書かれている入力を、2つのlong[]で受け取る
+        /// </summary>
+        /// <param name="rowNumber"></param>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="C"></param>
+        public void LongsArray(long rowNumber, ref long[] A, ref long[] B, ref long[] C)
+        {
+            A = new long[rowNumber];
+            B = new long[rowNumber];
+            C = new long[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                var l = ArrayLong();
+                A[i] = l[0];
+                B[i] = l[1];
+                C[i] = l[2];
+            }
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>long型の配列</returns>
+        public long[] ArrayLong()
+        {
+            return Split().Select(long.Parse).ToArray();
+        }
+
+        public long[] ArrayLong(int rowNumber)
+        {
+            long[] longs = new long[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                longs[i] = Long();
+            }
+
+            return longs;
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>double型の値</returns>
+        public double Double()
+        {
+            return double.Parse(String());
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>double型の配列</returns>
+        public double[] ArrayDouble()
+        {
+            return Split().Select(double.Parse).ToArray();
+        }
+
+        private IEnumerable<string> Split()
+        {
+            return String().Split(' ');
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solver solver = new Solver();
+            solver.Solve();
+        }
+    }
+
+    class Solver
+    {
+        public Solver()
+        {
+            Input input = new Input();
+            N = input.Long();
+            X = input.ArrayLong();
+            D = new long[N - 1];
+            for (int i = 0; i < N - 1; i++)
+            {
+                D[i] = X[i + 1] - X[i];
+            }
+        }
+
+        private long N;
+
+        private long[] X;
+
+        // 各スライムの距離
+        private long[] D;
+        private long MOD = 1000000007;
+
+        public void Solve()
+        {
+            var multi = getMulti(N - 1);
+            long ans = 0;
+            for (int i = 0; i < N - 1; i++)
+            {
+                ans = (ans + multi[i] * D[i] % MOD) % MOD;
+            }
+
+            Console.WriteLine(ans);
+        }
+
+        private Dictionary<long, long[]> Memo = new Dictionary<long, long[]>();
+
+        public long[] getMulti(long n)
+        {
+            if (Memo.ContainsKey(n))
+            {
+                return Memo[n];
+            }
+
+            if (n == 1)
+            {
+                long[] a = new long[1];
+                a[0] = 1;
+                Memo.Add(1, a);
+                return a;
+            }
+
+            long[] b = new long[n];
+            // 一つ選ぶときに移動させる距離の合計
+            for (int i = 0; i < n; i++)
+            {
+                b[i] = i + 1;
+            }
+
+            long[] l = getMulti(n - 1);
+
+            // i番目のスライムを合体させたとき
+            // i = 0は特別扱いする
+
+            for (int j = 0; j < n - 1; j++)
+            {
+                b[j + 1] += l[j];
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                // 1つ少ない数のスライムの組み合わせ方について、計算する
+                for (int j = 0; j < n; j++)
+                {
+                    if (j >= i)
+                    {
+                        b[j] += l[j - 1];
+                    }
+
+                    if (j < i)
+                    {
+                        b[j] += l[j];
+                    }
+                }
+            }
+
+            Memo.Add(n, b);
+            return b;
+        }
+    }
+}
