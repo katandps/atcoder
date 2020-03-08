1169ff4cc95d1e5e1e7160cedb9af5bafd0b5928
katand <katandps@gmail.com>
2020-03-08 20:56:01 +0900

    ABC158A-D

---
+++ src/ABC158E/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABC158E
+{
+    class Input
+    {
+        /// <summary>
+        /// 1行の入力を取得する
+        /// </summary>
+        /// <returns>文字列</returns>
+        public string String()
+        {
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 複数行の入力を取得
+        /// </summary>
+        /// <returns>文字列の配列</returns>
+        public string[] ArrayString(int rowNumber)
+        {
+            string[] s = new string[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                s[i] = String();
+            }
+
+            return s;
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>int型の値</returns>
+        public int Int()
+        {
+            return int.Parse(String());
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>int型の配列</returns>
+        public int[] ArrayInt()
+        {
+            return Split().Select(int.Parse).ToArray();
+        }
+
+        /// <summary>
+        /// 複数行の入力を取得
+        /// </summary>
+        /// <param name="rowNumber">行数</param>
+        /// <returns>int型の配列</returns>
+        public int[] ArrayInt(int rowNumber)
+        {
+            int[] ints = new int[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                ints[i] = Int();
+            }
+
+            return ints;
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>long型の値</returns>
+        public long Long()
+        {
+            return long.Parse(String());
+        }
+
+        /// <summary>
+        /// 2つの整数が1行に書かれている入力を、2つのlongで受け取る
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        public void Longs(ref long A, ref long B)
+        {
+            var longs = ArrayLong();
+            A = longs[0];
+            B = longs[1];
+        }
+
+        /// <summary>
+        /// 3つの整数が1行に書かれている入力を、3つのlongで受け取る
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="C"></param>
+        public void Longs(ref long A, ref long B, ref long C)
+        {
+            var longs = ArrayLong();
+            A = longs[0];
+            B = longs[1];
+            C = longs[2];
+        }
+        
+        /// <summary>
+        /// 4つの整数が1行に書かれている入力を、4つのlongで受け取る
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="C"></param>
+        /// <param name="D"></param>
+        public void Longs(ref long A, ref long B, ref long C, ref long D)
+        {
+            var longs = ArrayLong();
+            A = longs[0];
+            B = longs[1];
+            C = longs[2];
+            D = longs[3];
+        }
+
+        /// <summary>
+        /// 2つの整数が複数行に書かれている入力を、2つのlong[]で受け取る
+        /// </summary>
+        /// <param name="rowNumber"></param>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        public void LongsArray(long rowNumber, ref long[] A, ref long[] B)
+        {
+            A = new long[rowNumber];
+            B = new long[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                var l = ArrayLong();
+                A[i] = l[0];
+                B[i] = l[1];
+            }
+        }
+        
+        /// <summary>
+        /// 3つの整数が複数行に書かれている入力を、2つのlong[]で受け取る
+        /// </summary>
+        /// <param name="rowNumber"></param>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        /// <param name="C"></param>
+        public void LongsArray(long rowNumber, ref long[] A, ref long[] B, ref long[] C)
+        {
+            A = new long[rowNumber];
+            B = new long[rowNumber];
+            C = new long[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                var l = ArrayLong();
+                A[i] = l[0];
+                B[i] = l[1];
+                C[i] = l[2];
+            }
+        }
+        
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>long型の配列</returns>
+        public long[] ArrayLong()
+        {
+            return Split().Select(long.Parse).ToArray();
+        }
+
+        public long[] ArrayLong(int rowNumber)
+        {
+            long[] longs = new long[rowNumber];
+            for (int i = 0; i < rowNumber; i++)
+            {
+                longs[i] = Long();
+            }
+
+            return longs;
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>double型の値</returns>
+        public double Double()
+        {
+            return double.Parse(String());
+        }
+
+        /// <summary>
+        /// 1行の入力を取得
+        /// </summary>
+        /// <returns>double型の配列</returns>
+        public double[] ArrayDouble()
+        {
+            return Split().Select(double.Parse).ToArray();
+        }
+
+        private IEnumerable<string> Split()
+        {
+            return String().Split(' ');
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solver solver = new Solver();
+            solver.Solve();
+        }
+    }
+
+    class Solver
+    {
+        public Solver()
+        {
+            Input input = new Input();
+            input.Longs(ref N, ref P);
+            var s = input.String();
+            S = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                S[i] = int.Parse(s[i].ToString());
+            }
+        }
+
+        private long N;
+        private long P;
+        private int[] S;
+
+        public void Solve()
+        {
+            long[] memo = new long[P];
+            long[] tmp1 = new long[P];
+            
+            HashSet<long>[] set = new HashSet<long>[P];
+
+            for (int i = 0; i < P; i++)
+            {
+                set[i] = new HashSet<long>();
+            }
+
+            for (int i = 0; i < P; i++)
+            {
+                set[i * 10 % P].Add(i);
+            }
+            
+            long ans = 0;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < P; j++)
+                {
+                    var k = (j + S[i]) % P;
+                    tmp1[k] = 0;
+                    foreach (long l in set[k])
+                    {
+                        tmp1[k] += memo[l];
+                    }
+                }
+
+                tmp1[S[i] % P]++;
+                ans += tmp1[0];
+                memo = tmp1;
+            }
+            
+            Console.WriteLine(ans);
+        }
+    }
+}
