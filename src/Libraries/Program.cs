c30d34ae86cda9dcbed0cc9398416b088ec6f345
katand <katandps@gmail.com>
2020-03-09 11:35:29 +0900

    入力テンプレート修正 同じ型の値を入力する場合、同名のメソッドを使う

--- src/Libraries/Program.cs
+++ src/Libraries/Program.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 
-// namespaceの値をコンテスト名にして運用
-namespace Libraries
+namespace TaskName
 {
     class Input
     {
@@ -11,191 +10,178 @@ namespace Libraries
         /// 1行の入力を取得する
         /// </summary>
         /// <returns>文字列</returns>
-        public string String()
+        public void String(out string s)
         {
-            return Console.ReadLine();
+            s = Console.ReadLine();
         }
 
         /// <summary>
         /// 複数行の入力を取得
         /// </summary>
         /// <returns>文字列の配列</returns>
-        public string[] ArrayString(int rowNumber)
+        public void String(long rowNumber, out string[] s)
         {
-            string[] s = new string[rowNumber];
-            for (int i = 0; i < rowNumber; i++)
+            s = new String[rowNumber];
+            for (long i = 0; i < rowNumber; i++)
             {
-                s[i] = String();
+                String(out s[i]);
             }
-
-            return s;
         }
 
         /// <summary>
         /// 1行の入力を取得
         /// </summary>
         /// <returns>int型の値</returns>
-        public int Int()
+        public void Int(out int i)
         {
-            return int.Parse(String());
+            String(out string s);
+            i = int.Parse(s);
         }
 
         /// <summary>
         /// 1行の入力を取得
         /// </summary>
-        /// <returns>int型の配列</returns>
-        public int[] ArrayInt()
+        /// <param name="a"></param>
+        public void Long(out long a)
         {
-            return Split().Select(int.Parse).ToArray();
-        }
-
-        /// <summary>
-        /// 複数行の入力を取得
-        /// </summary>
-        /// <param name="rowNumber">行数</param>
-        /// <returns>int型の配列</returns>
-        public int[] ArrayInt(int rowNumber)
-        {
-            int[] ints = new int[rowNumber];
-            for (int i = 0; i < rowNumber; i++)
-            {
-                ints[i] = Int();
-            }
-
-            return ints;
-        }
-
-        /// <summary>
-        /// 1行の入力を取得
-        /// </summary>
-        /// <returns>long型の値</returns>
-        public long Long()
-        {
-            return long.Parse(String());
+            String(out string s);
+            a = long.Parse(s);
         }
 
         /// <summary>
         /// 2つの整数が1行に書かれている入力を、2つのlongで受け取る
         /// </summary>
-        /// <param name="A"></param>
-        /// <param name="B"></param>
-        public void Longs(ref long A, ref long B)
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void Long(out long a, out long b)
         {
-            var longs = ArrayLong();
-            A = longs[0];
-            B = longs[1];
+            Long(out long[] lArr);
+            a = lArr[0];
+            b = lArr[1];
         }
 
         /// <summary>
         /// 3つの整数が1行に書かれている入力を、3つのlongで受け取る
         /// </summary>
-        /// <param name="A"></param>
-        /// <param name="B"></param>
-        /// <param name="C"></param>
-        public void Longs(ref long A, ref long B, ref long C)
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        public void Long(out long a, out long b, out long c)
         {
-            var longs = ArrayLong();
-            A = longs[0];
-            B = longs[1];
-            C = longs[2];
+            Long(out long[] lArr);
+            a = lArr[0];
+            b = lArr[1];
+            c = lArr[2];
         }
-        
+
         /// <summary>
         /// 4つの整数が1行に書かれている入力を、4つのlongで受け取る
         /// </summary>
-        /// <param name="A"></param>
-        /// <param name="B"></param>
-        /// <param name="C"></param>
-        /// <param name="D"></param>
-        public void Longs(ref long A, ref long B, ref long C, ref long D)
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        public void Long(out long a, out long b, out long c, out long d)
         {
-            var longs = ArrayLong();
-            A = longs[0];
-            B = longs[1];
-            C = longs[2];
-            D = longs[3];
+            Long(out long[] lArr);
+            a = lArr[0];
+            b = lArr[1];
+            c = lArr[2];
+            d = lArr[3];
+        }
+
+        public void Long(out long[] lArr)
+        {
+            lArr = Split().Select(long.Parse).ToArray();
+        }
+
+        public void Long(long rowNumber, out long[] lArr)
+        {
+            lArr = new long[rowNumber];
+            for (long i = 0; i < rowNumber; i++)
+            {
+                Long(out lArr[i]);
+            }
         }
 
         /// <summary>
         /// 2つの整数が複数行に書かれている入力を、2つのlong[]で受け取る
         /// </summary>
         /// <param name="rowNumber"></param>
-        /// <param name="A"></param>
-        /// <param name="B"></param>
-        public void LongsArray(long rowNumber, ref long[] A, ref long[] B)
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public void Long(long rowNumber, out long[] a, out long[] b)
         {
-            A = new long[rowNumber];
-            B = new long[rowNumber];
+            a = new long[rowNumber];
+            b = new long[rowNumber];
             for (int i = 0; i < rowNumber; i++)
             {
-                var l = ArrayLong();
-                A[i] = l[0];
-                B[i] = l[1];
+                Long(out a[i], out b[i]);
             }
         }
-        
+
         /// <summary>
-        /// 3つの整数が複数行に書かれている入力を、2つのlong[]で受け取る
+        /// 3つの整数が複数行に書かれている入力を、3つのlong[]で受け取る
         /// </summary>
         /// <param name="rowNumber"></param>
-        /// <param name="A"></param>
-        /// <param name="B"></param>
-        /// <param name="C"></param>
-        public void LongsArray(long rowNumber, ref long[] A, ref long[] B, ref long[] C)
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        public void Long(long rowNumber, out long[] a, out long[] b, out long[] c)
         {
-            A = new long[rowNumber];
-            B = new long[rowNumber];
-            C = new long[rowNumber];
+            a = new long[rowNumber];
+            b = new long[rowNumber];
+            c = new long[rowNumber];
             for (int i = 0; i < rowNumber; i++)
             {
-                var l = ArrayLong();
-                A[i] = l[0];
-                B[i] = l[1];
-                C[i] = l[2];
+                Long(out a[i], out b[i], out c[i]);
             }
         }
-        
+
         /// <summary>
-        /// 1行の入力を取得
+        /// 4つの整数が複数行に書かれている入力を、4つのlong[]で受け取る
         /// </summary>
-        /// <returns>long型の配列</returns>
-        public long[] ArrayLong()
+        /// <param name="rowNumber"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="c"></param>
+        /// <param name="d"></param>
+        public void Long(long rowNumber, out long[] a, out long[] b, out long[] c, out long[] d)
         {
-            return Split().Select(long.Parse).ToArray();
-        }
-
-        public long[] ArrayLong(int rowNumber)
-        {
-            long[] longs = new long[rowNumber];
+            a = new long[rowNumber];
+            b = new long[rowNumber];
+            c = new long[rowNumber];
+            d = new long[rowNumber];
             for (int i = 0; i < rowNumber; i++)
             {
-                longs[i] = Long();
+                Long(out a[i], out b[i], out c[i], out d[i]);
             }
-
-            return longs;
         }
+
 
         /// <summary>
         /// 1行の入力を取得
         /// </summary>
         /// <returns>double型の値</returns>
-        public double Double()
+        public void Double(out double d)
         {
-            return double.Parse(String());
+            String(out var s);
+            d = double.Parse(s);
         }
 
         /// <summary>
         /// 1行の入力を取得
         /// </summary>
         /// <returns>double型の配列</returns>
-        public double[] ArrayDouble()
+        public void Double(out double[] dArr)
         {
-            return Split().Select(double.Parse).ToArray();
+            dArr = Split().Select(double.Parse).ToArray();
         }
 
         private IEnumerable<string> Split()
         {
-            return String().Split(' ');
+            String(out string s);
+            return s.Split(' ');
         }
     }
 
