75f9bf4680abaf9f76ce0a728dc3761211b76de3
katand <katandps@gmail.com>
2020-03-14 11:09:35 +0900

    あさかつ2020/03/14

---
+++ src/Asakatsu20200314A/Local.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Asakatsu20200314A
+{
+    public class Local
+    {
+        static void Main(string[] args)
+        {
+            var sw = new Stopwatch();
+            sw.Start();
+            Console.SetIn(new StreamReader("../../../input.in"));
+            Program.Main(args);
+            Console.WriteLine("{0}ms", sw.ElapsedMilliseconds);
+        }
+    }
+}
