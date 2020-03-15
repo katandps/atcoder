9bc2e5c50540ee257a2a65fa095ff4de41a52b8f
katand <katandps@gmail.com>
2020-03-15 15:11:29 +0900

    ABC066 A-D

---
+++ src/ABC066_20200315B/Local.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ABC066_20200315B
+{
+    public class Local
+    {
+        private const string Filename = "out.txt";
+
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Console.SetIn(new StreamReader("../../../input.in"));
+
+            FileStream writeFs = new FileStream(Filename, FileMode.Create);
+            StreamWriter fileSw = new StreamWriter(writeFs) {AutoFlush = false};
+            Console.SetOut(fileSw);
+
+            Program.Debug();
+            Console.WriteLine("{0}ms", stopWatch.ElapsedMilliseconds);
+            Console.Out.Flush();
+            writeFs.Close();
+
+            FileStream readFs = new FileStream(Filename, FileMode.Open);
+            StreamReader sr = new StreamReader(readFs);
+            StreamWriter standard = new StreamWriter(Console.OpenStandardOutput()) {AutoFlush = false};
+            Console.SetOut(standard);
+            Console.WriteLine(sr.ReadToEnd());
+            Console.Out.Flush();
+            readFs.Close();
+        }
+    }
+}
---
+++ src/ABC066_20200315D/Program.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static ABC066_20200315D.Input;
+
+namespace ABC066_20200315D
+{
+    class Input
+    {
+        /// <summary>
+        /// 1行の文字列の入力
+        /// </summary>
+        public static void @in(out string s)
+        {
+            s = String();
+        }
+
+        private static String String()
+        {
+            return Console.ReadLine();
+        }
+
+        /// <summary>
+        /// 複数行の文字列の入力
+        /// </summary>
+        public static void @in(long rowNumber, out string[] s)
+        {
+            s = new String[rowNumber];
+            for (long i = 0; i < rowNumber; i++) @in(out s[i]);
+        }
+
+        /// <summary>
+        /// 1行に書かれた1つの整数の入力
+        /// </summary>
+        public static void @in(out int i)
+        {
+            i = int.Parse(String());
+        }
+
+        /// <summary>
+        /// 1行に書かれた1つの整数の入力
+        /// </summary>
+        public static void @in(out long a)
+        {
+            a = Long()[0];
+        }
+
+        /// <summary>
+        /// 1行に書かれた2つの整数の入力
+        /// </summary>
+        public static void @in(out long a, out long b)
+        {
+            long[] lArr = Long();
+            a = lArr[0];
+            b = lArr[1];
+        }
+
+        /// <summary>
+        /// 1行に書かれた3つの整数の入力
+        /// </summary>
+        public static void @in(out long a, out long b, out long c)
+        {
+            long[] lArr = Long();
+            a = lArr[0];
+            b = lArr[1];
+            c = lArr[2];
+        }
+
+        /// <summary>
+        /// 1行に書かれた4つの整数の入力
+        /// </summary>
+        public static void @in(out long a, out long b, out long c, out long d)
+        {
+            long[] lArr = Long();
+            a = lArr[0];
+            b = lArr[1];
+            c = lArr[2];
+            d = lArr[3];
+        }
+
+        /// <summary>
+        /// 1行に書かれた5つの整数の入力
+        /// </summary>
+        public static void @in(out long a, out long b, out long c, out long d, out long e)
+        {
+            long[] lArr = Long();
+            a = lArr[0];
+            b = lArr[1];
+            c = lArr[2];
+            d = lArr[3];
+            e = lArr[4];
+        }
+
+        private static long[] Long()
+        {
+            return Split().Select(long.Parse).ToArray();
+        }
+
+        /// <summary>
+        /// 1行に書かれた複数の整数列の入力
+        /// </summary>
+        public static void @in(out long[] lArr)
+        {
+            lArr = Long();
+        }
+
+        /// <summary>
+        /// rowNumber行に書かれた1つの整数列の入力
+        /// </summary>
+        public static void @in(long rowNumber, out long[] lArr)
+        {
+            lArr = new long[rowNumber];
+            for (long i = 0; i < rowNumber; i++) @in(out lArr[i]);
+        }
+
+        /// <summary>
+        /// rowNumber行に書かれた2つの整数列の入力
+        /// </summary>
+        public static void @in(long rowNumber, out long[] a, out long[] b)
+        {
+            a = new long[rowNumber];
+            b = new long[rowNumber];
+            for (int i = 0; i < rowNumber; i++) @in(out a[i], out b[i]);
+        }
+
+        /// <summary>
+        /// rowNumber行に書かれた3つの整数列の入力
+        /// </summary>
+        public static void @in(long rowNumber, out long[] a, out long[] b, out long[] c)
+        {
+            a = new long[rowNumber];
+            b = new long[rowNumber];
+            c = new long[rowNumber];
+            for (int i = 0; i < rowNumber; i++) @in(out a[i], out b[i], out c[i]);
+        }
+
+        /// <summary>
+        /// rowNumber行に書かれた4つの整数列の入力
+        /// </summary>
+        public static void @in(long rowNumber, out long[] a, out long[] b, out long[] c, out long[] d)
+        {
+            a = new long[rowNumber];
+            b = new long[rowNumber];
+            c = new long[rowNumber];
+            d = new long[rowNumber];
+            for (int i = 0; i < rowNumber; i++) @in(out a[i], out b[i], out c[i], out d[i]);
+        }
+
+        /// <summary>
+        /// h行w列の整数の行列の入力
+        /// </summary>
+        public static void @in(long h, long w, out long[][] a)
+        {
+            a = new long[h][];
+            for (long i = 0; i < h; i++) @in(out a[i]);
+        }
+
+        /// <summary>
+        /// 1行に書かれた1つの小数の入力
+        /// </summary>
+        public static void @in(out double d)
+        {
+            d = double.Parse(String());
+        }
+
+        /// <summary>
+        /// 1行に書かれた小数の配列の入力
+        /// </summary>
+        public static void @in(out double[] dArr)
+        {
+            dArr = Split().Select(double.Parse).ToArray();
+        }
+
+        private static IEnumerable<string> Split()
+        {
+            return String().Split(' ');
+        }
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StreamWriter streamWriter = new StreamWriter(Console.OpenStandardOutput()) {AutoFlush = false};
+            Console.SetOut(streamWriter);
+            new Solver().Solve();
+            Console.Out.Flush();
+        }
+
+        public static void Debug()
+        {
+            new Solver().Solve();
+        }
+    }
+
+    class Solver
+    {
+        private long N;
+        private long[] A;
+
+        public void Solve()
+        {
+            @in(out N);
+            @in(out A);
+
+            int l = -1;
+            int r = -1;
+            Dictionary<long, int> index = new Dictionary<long, int>();
+            for (int i = 0; i < N + 1; i++)
+            {
+                if (index.ContainsKey(A[i]))
+                {
+                    l = index[A[i]];
+                    r = (int) N - i;
+                    break;
+                }
+
+                index.Add(A[i], i);
+            }
+
+            Console.WriteLine(N);
+            for (int i = 2; i <= N + 1; i++)
+            {
+                Modular a = Modular.Ncr((int) N + 1, i);
+                if (i == 2)
+                {
+                    a -= l + r;
+                }
+                else
+                {
+                    if (l > 0 && r > 0)
+                    {
+                        a -= Modular.Ncr(l + r, i - 1);
+                    }
+                    else if (l >= i - 1)
+                    {
+                        a -= Modular.Ncr(l, i);
+                    }
+                    else if (r >= i - 1)
+                    {
+                        a -= Modular.Ncr(r, i);
+                    }
+                }
+
+                Console.WriteLine((int) a);
+            }
+        }
+    }
+
+    class Modular
+    {
+        public static int M = 1000000007;
+        private long V;
+
+        public Modular(long v)
+        {
+            V = v;
+        }
+
+        public static implicit operator Modular(long a)
+        {
+            var m = a % M;
+            return new Modular(m < 0 ? m + M : m);
+        }
+
+        public static Modular operator +(Modular a, Modular b)
+        {
+            return a.V + b.V;
+        }
+
+        public static Modular operator -(Modular a, Modular b)
+        {
+            return a.V - b.V;
+        }
+
+        public static Modular operator *(Modular a, Modular b)
+        {
+            return a.V * b.V;
+        }
+
+        public static Modular Pow(Modular a, int n)
+        {
+            switch (n)
+            {
+                case 0:
+                    return 1;
+                case 1:
+                    return a;
+                default:
+                    var p = Pow(a, n / 2);
+                    return p * p * Pow(a, n % 2);
+            }
+        }
+
+        public static Modular operator /(Modular a, Modular b)
+        {
+            return a * Pow(b, M - 2);
+        }
+
+        private static readonly List<int> Facts = new List<int> {1};
+
+        public static Modular Fac(int n)
+        {
+            for (int i = Facts.Count; i <= n; ++i)
+            {
+                Facts.Add((int) (Math.BigMul(Facts.Last(), i) % M));
+            }
+
+            return Facts[n];
+        }
+
+        public static Modular Ncr(int n, int r)
+        {
+            if (n < r)
+            {
+                return 0;
+            }
+
+            if (n == r)
+            {
+                return 1;
+            }
+
+            return Fac(n) / (Fac(r) * Fac(n - r));
+        }
+
+        public static explicit operator int(Modular a)
+        {
+            return (int) a.V;
+        }
+    }
+}
