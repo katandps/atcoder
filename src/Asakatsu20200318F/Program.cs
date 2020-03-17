9bc67f52c2c225ad6624da498ee33240dd93102f
katand <katandps@gmail.com>
2020-03-18 08:46:00 +0900

    あさかつ2020/03/18

---
+++ src/Asakatsu20200318F/Program.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static Asakatsu20200318F.Input;
+
+namespace Asakatsu20200318F
+{
+    static class Input
+    {
+        /// <summary>
+        /// 1行の入力をTに応じてリストに変換する関数を返す
+        /// </summary>
+        static Func<string, List<T>> Cast<T>() => _ => _.Split(' ').Select(Convert<T>()).ToList();
+
+        /// <summary>
+        /// 1行の入力をTに応じて変換する関数を返す
+        /// </summary>
+        static Func<string, T> Convert<T>()
+        {
+            Type t = typeof(T);
+            if (t == typeof(string)) return _ => (T) (object) _;
+            if (t == typeof(int)) return _ => (T) (object) int.Parse(_);
+            if (t == typeof(long)) return _ => (T) (object) long.Parse(_);
+            if (t == typeof(double)) return _ => (T) (object) double.Parse(_);
+            if (t == typeof(List<string>)) return _ => (T) (object) Cast<string>()(_);
+            if (t == typeof(List<int>)) return _ => (T) (object) Cast<int>()(_);
+            if (t == typeof(List<long>)) return _ => (T) (object) Cast<long>()(_);
+            if (t == typeof(List<double>)) return _ => (T) (object) Cast<double>()(_);
+
+            throw new NotSupportedException(t + "is not supported.");
+        }
+
+        static string String()
+        {
+            var s = Console.ReadLine();
+            if (s == null) throw new Exception("入力がありません");
+            return s;
+        }
+
+        static List<string> String(long rowNumber) => new bool[rowNumber].Select(_ => String()).ToList();
+
+        /// <summary>
+        /// 1行の値の入力
+        /// </summary>
+        public static void @in<T>(out T a) => a = Convert<List<T>>()(String())[0];
+
+        public static void @in<T>(out T a1, out T a2)
+        {
+            var v = Convert<List<T>>()(String());
+            a1 = v[0];
+            a2 = v[1];
+        }
+
+        public static void @in<T>(out T a1, out T a2, out T a3)
+        {
+            var v = Convert<List<T>>()(String());
+            a1 = v[0];
+            a2 = v[1];
+            a3 = v[2];
+        }
+
+        public static void @in<T>(out T a1, out T a2, out T a3, out T a4)
+        {
+            var v = Convert<List<T>>()(String());
+            a1 = v[0];
+            a2 = v[1];
+            a3 = v[2];
+            a4 = v[3];
+        }
+
+        public static void @in<T>(out T a1, out T a2, out T a3, out T a4, out T a5)
+        {
+            var v = Convert<List<T>>()(String());
+            a1 = v[0];
+            a2 = v[1];
+            a3 = v[2];
+            a4 = v[3];
+            a5 = v[4];
+        }
+
+        public static void @in<T>(out T a1, out T a2, out T a3, out T a4, out T a5, out T a6)
+        {
+            var v = Convert<List<T>>()(String());
+            a1 = v[0];
+            a2 = v[1];
+            a3 = v[2];
+            a4 = v[3];
+            a5 = v[4];
+            a6 = v[5];
+        }
+
+        /// <summary>
+        /// 複数行の値の入力
+        /// </summary>
+        public static void @in<T>(long rowNumber, out List<T> l) => l = String(rowNumber).Select(Convert<T>()).ToList();
+
+        public static void @in<T>(long rowNumber, out List<T> l1, out List<T> l2)
+        {
+            l1 = new List<T>();
+            l2 = new List<T>();
+            foreach (List<T> l in String(rowNumber).Select(Convert<List<T>>()))
+            {
+                l1.Add(l[0]);
+                l2.Add(l[1]);
+            }
+        }
+
+        public static void @in<T>(long rowNumber, out List<T> l1, out List<T> l2, out List<T> l3)
+        {
+            l1 = new List<T>();
+            l2 = new List<T>();
+            l3 = new List<T>();
+            foreach (List<T> l in String(rowNumber).Select(Convert<List<T>>()))
+            {
+                l1.Add(l[0]);
+                l2.Add(l[1]);
+                l3.Add(l[2]);
+            }
+        }
+
+        public static void @in<T>(long rowNumber, out List<T> l1, out List<T> l2, out List<T> l3, out List<T> l4)
+        {
+            l1 = new List<T>();
+            l2 = new List<T>();
+            l3 = new List<T>();
+            l4 = new List<T>();
+            foreach (List<T> l in String(rowNumber).Select(Convert<List<T>>()))
+            {
+                l1.Add(l[0]);
+                l2.Add(l[1]);
+                l3.Add(l[2]);
+                l4.Add(l[3]);
+            }
+        }
+
+        /// <summary>
+        /// 1行に書かれた複数の値の入力
+        /// </summary>
+        public static void @in<T>(out List<T> lArr) => lArr = Convert<List<T>>()(String());
+
+        /// <summary>
+        /// h行の行列の入力
+        /// </summary>
+        public static void @in<T>(long h, out List<List<T>> map) => map = String(h).Select(Convert<List<T>>()).ToList();
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StreamWriter streamWriter = new StreamWriter(Console.OpenStandardOutput()) {AutoFlush = false};
+            Console.SetOut(streamWriter);
+            new Solver().Solve();
+            Console.Out.Flush();
+        }
+
+        public static void Debug()
+        {
+            new Solver().Solve();
+        }
+    }
+
+    class Solver
+    {
+        private int N;
+        private List<int> P;
+
+        public void Solve()
+        {
+            @in(out N);
+            @in(out P);
+
+            int[] X = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                X[P[i] - 1] = 999900000 + i;
+            }
+
+            int[] A = new int[N];
+            int[] B = new int[N];
+
+            A[0] = 1;
+            B[0] = X[0] - 1;
+
+            for (int i = 1; i < N; i++)
+            {
+                A[i] = Math.Max(A[i - 1] + 1, X[i] - B[i - 1] + 1);
+                B[i] = X[i] - A[i];
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("" + A[i] + " ");
+            }
+
+            Console.WriteLine();
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write("" + B[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
