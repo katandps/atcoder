1f0e14d6db01a2083a2e401330b39b3295e0d27f
katand <katandps@gmail.com>
2020-03-19 17:57:25 +0900

    行の読み取りの型について、より柔軟になるように変更

--- src/Libraries/Program.cs
+++ src/Libraries/Program.cs
@@ -31,6 +31,8 @@ namespace Libraries
             throw new NotSupportedException(t + "is not supported.");
         }
 
+        static T Convert<T>(string s) => Convert<T>()(s);
+
         static string String() => Console.ReadLine();
 
         static List<string> String(long rowNumber) => new bool[rowNumber].Select(_ => String()).ToList();
@@ -38,108 +40,107 @@ namespace Libraries
         /// <summary>
         /// 1行の値の入力
         /// </summary>
-        public static void @in<T>(out T a) => a = Convert<List<T>>()(String())[0];
+        public static void Cin<T>(out T a) => a = Convert<List<T>>(String())[0];
 
-        public static void @in<T>(out T a1, out T a2)
+        public static void Cin<T1, T2>(out T1 a1, out T2 a2)
         {
-            var v = Convert<List<T>>()(String());
-            a1 = v[0];
-            a2 = v[1];
+            var v = Convert<List<string>>(String());
+            a1 = Convert<T1>(v[0]);
+            a2 = Convert<T2>(v[1]);
         }
 
-        public static void @in<T>(out T a1, out T a2, out T a3)
+        public static void Cin<T1, T2, T3>(out T1 a1, out T2 a2, out T3 a3)
         {
-            var v = Convert<List<T>>()(String());
-            a1 = v[0];
-            a2 = v[1];
-            a3 = v[2];
+            var v = Convert<List<string>>(String());
+            a1 = Convert<T1>(v[0]);
+            a2 = Convert<T2>(v[1]);
+            a3 = Convert<T3>(v[2]);
         }
 
-        public static void @in<T>(out T a1, out T a2, out T a3, out T a4)
+        public static void Cin<T1, T2, T3, T4>(out T1 a1, out T2 a2, out T3 a3, out T4 a4)
         {
-            var v = Convert<List<T>>()(String());
-            a1 = v[0];
-            a2 = v[1];
-            a3 = v[2];
-            a4 = v[3];
+            var v = Convert<List<string>>(String());
+            a1 = Convert<T1>(v[0]);
+            a2 = Convert<T2>(v[1]);
+            a3 = Convert<T3>(v[2]);
+            a4 = Convert<T4>(v[3]);
         }
 
-        public static void @in<T>(out T a1, out T a2, out T a3, out T a4, out T a5)
+        public static void Cin<T1, T2, T3, T4, T5>(out T1 a1, out T2 a2, out T3 a3, out T4 a4, out T5 a5)
         {
-            var v = Convert<List<T>>()(String());
-            a1 = v[0];
-            a2 = v[1];
-            a3 = v[2];
-            a4 = v[3];
-            a5 = v[4];
+            var v = Convert<List<string>>(String());
+            a1 = Convert<T1>(v[0]);
+            a2 = Convert<T2>(v[1]);
+            a3 = Convert<T3>(v[2]);
+            a4 = Convert<T4>(v[3]);
+            a5 = Convert<T5>(v[4]);
         }
 
-        public static void @in<T>(out T a1, out T a2, out T a3, out T a4, out T a5, out T a6)
+        public static void Cin<T1, T2, T3, T4, T5, T6>(out T1 a1, out T2 a2, out T3 a3, out T4 a4, out T5 a5, out T6 a6)
         {
-            var v = Convert<List<T>>()(String());
-            a1 = v[0];
-            a2 = v[1];
-            a3 = v[2];
-            a4 = v[3];
-            a5 = v[4];
-            a6 = v[5];
+            var v = Convert<List<string>>(String());
+            a1 = Convert<T1>(v[0]);
+            a2 = Convert<T2>(v[1]);
+            a3 = Convert<T3>(v[2]);
+            a4 = Convert<T4>(v[3]);
+            a5 = Convert<T5>(v[4]);
+            a6 = Convert<T6>(v[5]);
         }
 
         /// <summary>
         /// 複数行の値の入力
         /// </summary>
-        public static void @in<T>(long rowNumber, out List<T> l) => l = String(rowNumber).Select(Convert<T>()).ToList();
+        public static void Cin<T>(long n, out List<T> l) => l = String(n).Select(Convert<T>()).ToList();
 
-        public static void @in<T1, T2>(long rowNumber, out List<T1> l1, out List<T2> l2)
+        public static void Cin<T1, T2>(long n, out List<T1> l1, out List<T2> l2)
         {
             l1 = new List<T1>();
             l2 = new List<T2>();
-            foreach (List<string> l in String(rowNumber).Select(Convert<List<string>>()))
+            foreach (List<string> l in String(n).Select(Convert<List<string>>()))
             {
-                l1.Add(Convert<T1>()(l[0]));
-                l2.Add(Convert<T2>()(l[1]));
+                l1.Add(Convert<T1>(l[0]));
+                l2.Add(Convert<T2>(l[1]));
             }
         }
 
-        public static void @in<T1, T2, T3>(long rowNumber, out List<T1> l1, out List<T2> l2, out List<T3> l3)
+        public static void Cin<T1, T2, T3>(long n, out List<T1> l1, out List<T2> l2, out List<T3> l3)
         {
             l1 = new List<T1>();
             l2 = new List<T2>();
             l3 = new List<T3>();
-            foreach (List<string> l in String(rowNumber).Select(Convert<List<string>>()))
+            foreach (List<string> l in String(n).Select(Convert<List<string>>()))
             {
-                l1.Add(Convert<T1>()(l[0]));
-                l2.Add(Convert<T2>()(l[1]));
-                l3.Add(Convert<T3>()(l[2]));
+                l1.Add(Convert<T1>(l[0]));
+                l2.Add(Convert<T2>(l[1]));
+                l3.Add(Convert<T3>(l[2]));
             }
         }
 
-        public static void @in<T1, T2, T3, T4>(long rowNumber, out List<T1> l1, out List<T2> l2, out List<T3> l3,
+        public static void Cin<T1, T2, T3, T4>(long n, out List<T1> l1, out List<T2> l2, out List<T3> l3,
             out List<T4> l4)
         {
             l1 = new List<T1>();
             l2 = new List<T2>();
             l3 = new List<T3>();
             l4 = new List<T4>();
-            foreach (List<string> l in String(rowNumber).Select(Convert<List<string>>()))
+            foreach (List<string> l in String(n).Select(Convert<List<string>>()))
             {
-                l1.Add(Convert<T1>()(l[0]));
-                l2.Add(Convert<T2>()(l[1]));
-                l3.Add(Convert<T3>()(l[2]));
-                l4.Add(Convert<T4>()(l[3]));
+                l1.Add(Convert<T1>(l[0]));
+                l2.Add(Convert<T2>(l[1]));
+                l3.Add(Convert<T3>(l[2]));
+                l4.Add(Convert<T4>(l[3]));
             }
         }
-
 
         /// <summary>
         /// 1行に書かれた複数の値の入力
         /// </summary>
-        public static void @in<T>(out List<T> lArr) => lArr = Convert<List<T>>()(String());
+        public static void Cin<T>(out List<T> lArr) => lArr = Convert<List<T>>(String());
 
         /// <summary>
         /// h行の行列の入力
         /// </summary>
-        public static void @in<T>(long h, out List<List<T>> map) => map = String(h).Select(Convert<List<T>>()).ToList();
+        public static void Cin<T>(long h, out List<List<T>> map) => map = String(h).Select(Convert<List<T>>()).ToList();
     }
 
     class Program
