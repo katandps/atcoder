12298100d1672d8cc9454c371ee0998e625fdb63
katand <katandps@gmail.com>
2020-03-22 13:04:35 +0900

    入力ライブラリの出力をListからArrayに変更

--- src/Libraries/Program.cs
+++ src/Libraries/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using static Libraries.Input;
@@ -8,89 +7,79 @@ namespace Libraries
 {
     static class Input
     {
-        /// <summary>
-        /// 1行の入力をTに応じてリストに変換する関数を返す
-        /// </summary>
-        static Func<string, List<T>> Cast<T>() => _ => _.Split(' ').Select(Convert<T>()).ToList();
+        private static Func<string, T[]> Cast<T>() => _ => _.Split(' ').Select(Convert<T>()).ToArray();
 
-        /// <summary>
-        /// 1行の入力をTに応じて変換する関数を返す
-        /// </summary>
-        static Func<string, T> Convert<T>()
+        private static Func<string, T> Convert<T>()
         {
             Type t = typeof(T);
             if (t == typeof(string)) return _ => (T) (object) _;
             if (t == typeof(int)) return _ => (T) (object) int.Parse(_);
             if (t == typeof(long)) return _ => (T) (object) long.Parse(_);
             if (t == typeof(double)) return _ => (T) (object) double.Parse(_);
-            if (t == typeof(List<string>)) return _ => (T) (object) Cast<string>()(_);
-            if (t == typeof(List<int>)) return _ => (T) (object) Cast<int>()(_);
-            if (t == typeof(List<long>)) return _ => (T) (object) Cast<long>()(_);
-            if (t == typeof(List<double>)) return _ => (T) (object) Cast<double>()(_);
+            if (t == typeof(string[])) return _ => (T) (object) Cast<string>()(_);
+            if (t == typeof(int[])) return _ => (T) (object) Cast<int>()(_);
+            if (t == typeof(long[])) return _ => (T) (object) Cast<long>()(_);
+            if (t == typeof(double[])) return _ => (T) (object) Cast<double>()(_);
 
             throw new NotSupportedException(t + "is not supported.");
         }
 
-        static T Convert<T>(string s) => Convert<T>()(s);
+        private static T Convert<T>(string s) => Convert<T>()(s);
 
-        static string String() => Console.ReadLine();
+        private static string String() => Console.ReadLine();
 
-        static List<string> String(long rowNumber) => new bool[rowNumber].Select(_ => String()).ToList();
+        private static string[] String(long rowNumber) => new bool[rowNumber].Select(_ => String()).ToArray();
 
-        /// <summary>
-        /// 1行の値の入力
-        /// </summary>
-        public static void Cin<T>(out T a) => a = Convert<List<T>>(String())[0];
+        public static void Cin<T>(out T a) => a = Convert<T>(String());
 
         public static void Cin<T1, T2>(out T1 a1, out T2 a2)
         {
-            var v = Convert<List<string>>(String());
-            a1 = Convert<T1>(v[0]);
-            a2 = Convert<T2>(v[1]);
+            var v = Convert<string[]>(String());
+            set(v[0], out a1);
+            set(v[1], out a2);
         }
 
         public static void Cin<T1, T2, T3>(out T1 a1, out T2 a2, out T3 a3)
         {
-            var v = Convert<List<string>>(String());
-            a1 = Convert<T1>(v[0]);
-            a2 = Convert<T2>(v[1]);
-            a3 = Convert<T3>(v[2]);
+            var v = Convert<string[]>(String());
+            set(v[0], out a1);
+            set(v[1], out a2);
+            set(v[2], out a3);
         }
 
         public static void Cin<T1, T2, T3, T4>(out T1 a1, out T2 a2, out T3 a3, out T4 a4)
         {
-            var v = Convert<List<string>>(String());
-            a1 = Convert<T1>(v[0]);
-            a2 = Convert<T2>(v[1]);
-            a3 = Convert<T3>(v[2]);
-            a4 = Convert<T4>(v[3]);
+            var v = Convert<string[]>(String());
+            set(v[0], out a1);
+            set(v[1], out a2);
+            set(v[2], out a3);
+            set(v[3], out a4);
         }
 
         public static void Cin<T1, T2, T3, T4, T5>(out T1 a1, out T2 a2, out T3 a3, out T4 a4, out T5 a5)
         {
-            var v = Convert<List<string>>(String());
-            a1 = Convert<T1>(v[0]);
-            a2 = Convert<T2>(v[1]);
-            a3 = Convert<T3>(v[2]);
-            a4 = Convert<T4>(v[3]);
-            a5 = Convert<T5>(v[4]);
+            var v = Convert<string[]>(String());
+            set(v[0], out a1);
+            set(v[1], out a2);
+            set(v[2], out a3);
+            set(v[3], out a4);
+            set(v[4], out a5);
         }
 
         public static void Cin<T1, T2, T3, T4, T5, T6>(out T1 a1, out T2 a2, out T3 a3, out T4 a4, out T5 a5, out T6 a6)
         {
-            var v = Convert<List<string>>(String());
-            a1 = Convert<T1>(v[0]);
-            a2 = Convert<T2>(v[1]);
-            a3 = Convert<T3>(v[2]);
-            a4 = Convert<T4>(v[3]);
-            a5 = Convert<T5>(v[4]);
-            a6 = Convert<T6>(v[5]);
+            var v = Convert<string[]>(String());
+            set(v[0], out a1);
+            set(v[1], out a2);
+            set(v[2], out a3);
+            set(v[3], out a4);
+            set(v[4], out a5);
+            set(v[5], out a6);
         }
 
-        /// <summary>
-        /// 複数行の値の入力
-        /// </summary>
-        public static void Cin<T>(long n, out List<T> l) => l = String(n).Select(Convert<T>()).ToList();
+        private static void set<T1>(string s, out T1 o1) => o1 = Convert<T1>(s);
+
+        public static void Cin<T>(long n, out T[] l) => l = String(n).Select(Convert<T>()).ToArray();
 
         public static void Cin<T1, T2>(long n, out T1[] l1, out T2[] l2)
         {
@@ -116,15 +105,9 @@ namespace Libraries
             for (int i = 0; i < n; i++) Cin(out l1[i], out l2[i], out l3[i], out l4[i]);
         }
 
-        /// <summary>
-        /// 1行に書かれた複数の値の入力
-        /// </summary>
-        public static void Cin<T>(out List<T> lArr) => lArr = Convert<List<T>>(String());
+        public static void Cin<T>(out T[] a) => a = Convert<T[]>(String());
 
-        /// <summary>
-        /// h行の行列の入力
-        /// </summary>
-        public static void Cin<T>(long h, out List<List<T>> map) => map = String(h).Select(Convert<List<T>>()).ToList();
+        public static void Cin<T>(long h, out T[][] map) => map = String(h).Select(Convert<T[]>()).ToArray();
     }
 
     class Program
