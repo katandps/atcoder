9c761eba86b4abeb72b1e454f5fd37911450f611
katand <katandps@gmail.com>
2020-04-04 23:39:20 +0900

    ABC161 A-F

---
+++ src/ABC161F/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static ABC161F.Input;
+
+namespace ABC161F
+{
+    static class Input
+    {
+        private static Func<string, T[]> Cast<T>() => _ => _.Split(' ').Select(Convert<T>()).ToArray();
+
+        private static Func<string, T> Convert<T>()
+        {
+            Type t = typeof(T);
+            if (t == typeof(string)) return _ => (T) (object) _;
+            if (t == typeof(int)) return _ => (T) (object) int.Parse(_);
+            if (t == typeof(long)) return _ => (T) (object) long.Parse(_);
+            if (t == typeof(double)) return _ => (T) (object) double.Parse(_);
+            if (t == typeof(string[])) return _ => (T) (object) _.Split(' ');
+            if (t == typeof(int[])) return _ => (T) (object) Cast<int>()(_);
+            if (t == typeof(long[])) return _ => (T) (object) Cast<long>()(_);
+            if (t == typeof(double[])) return _ => (T) (object) Cast<double>()(_);
+
+            throw new NotSupportedException(t + "is not supported.");
+        }
+
+        private static T Convert<T>(string s) => Convert<T>()(s);
+
+        private static string String() => Console.ReadLine();
+
+        private static string[] String(long rowNumber) => new bool[rowNumber].Select(_ => String()).ToArray();
+
+        public static void Cin<T>(out T a) => a = Convert<T>(String());
+
+        public static void Cin<T1, T2>(out T1 a1, out T2 a2)
+        {
+            var v = String().Split(' ');
+            set(v[0], out a1);
+            set(v[1], out a2);
+        }
+
+        public static void Cin<T1, T2, T3>(out T1 a1, out T2 a2, out T3 a3)
+        {
+            var v = String().Split(' ');
+            set(v[0], out a1);
+            set(v[1], out a2);
+            set(v[2], out a3);
+        }
+
+        public static void Cin<T1, T2, T3, T4>(out T1 a1, out T2 a2, out T3 a3, out T4 a4)
+        {
+            var v = String().Split(' ');
+            set(v[0], out a1);
+            set(v[1], out a2);
+            set(v[2], out a3);
+            set(v[3], out a4);
+        }
+
+        public static void Cin<T1, T2, T3, T4, T5>(out T1 a1, out T2 a2, out T3 a3, out T4 a4, out T5 a5)
+        {
+            var v = String().Split(' ');
+            set(v[0], out a1);
+            set(v[1], out a2);
+            set(v[2], out a3);
+            set(v[3], out a4);
+            set(v[4], out a5);
+        }
+
+        public static void Cin<T1, T2, T3, T4, T5, T6>(out T1 a1, out T2 a2, out T3 a3, out T4 a4, out T5 a5, out T6 a6)
+        {
+            var v = String().Split(' ');
+            set(v[0], out a1);
+            set(v[1], out a2);
+            set(v[2], out a3);
+            set(v[3], out a4);
+            set(v[4], out a5);
+            set(v[5], out a6);
+        }
+
+        private static void set<T1>(string s, out T1 o1) => o1 = Convert<T1>(s);
+
+        public static void Cin<T>(long n, out T[] l) => l = String(n).Select(Convert<T>()).ToArray();
+
+        public static void Cin<T1, T2>(long n, out T1[] l1, out T2[] l2)
+        {
+            l1 = new T1[n];
+            l2 = new T2[n];
+            for (int i = 0; i < n; i++) Cin(out l1[i], out l2[i]);
+        }
+
+        public static void Cin<T1, T2, T3>(long n, out T1[] l1, out T2[] l2, out T3[] l3)
+        {
+            l1 = new T1[n];
+            l2 = new T2[n];
+            l3 = new T3[n];
+            for (int i = 0; i < n; i++) Cin(out l1[i], out l2[i], out l3[i]);
+        }
+
+        public static void Cin<T1, T2, T3, T4>(long n, out T1[] l1, out T2[] l2, out T3[] l3, out T4[] l4)
+        {
+            l1 = new T1[n];
+            l2 = new T2[n];
+            l3 = new T3[n];
+            l4 = new T4[n];
+            for (int i = 0; i < n; i++) Cin(out l1[i], out l2[i], out l3[i], out l4[i]);
+        }
+
+        public static void Cin<T>(out T[] a) => a = Convert<T[]>(String());
+
+        public static void Cin<T>(long h, out T[][] map) => map = String(h).Select(Convert<T[]>()).ToArray();
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            StreamWriter streamWriter = new StreamWriter(Console.OpenStandardOutput()) {AutoFlush = false};
+            Console.SetOut(streamWriter);
+            new Solver().Solve();
+            Console.Out.Flush();
+        }
+
+        public static void Debug()
+        {
+            new Solver().Solve();
+        }
+    }
+
+    class Solver
+    {
+        public void Solve()
+        {
+            long n;
+            Cin(out n);
+
+
+            long l = 1;
+            HashSet<long> divisors = new HashSet<long>();
+            while (l * l <= n)
+            {
+                if (n % l == 0)
+                {
+                    divisors.Add(l);
+                    divisors.Add(n / l);
+                    if (l == n / l)
+                    {
+                        ans.Add(l);
+                    }
+                }
+
+                l++;
+            }
+
+            divisors.Remove(1);
+
+            foreach (long d in divisors)
+            {
+                long k = n;
+                while (k % d == 0) k /= d;
+                if (k % d == 1)
+                {
+                    ans.Add(d);
+                }
+            }
+
+            add(n - 1);
+            ans.Remove(1);
+
+            Console.WriteLine(ans.Count);
+        }
+
+        void add(long n)
+        {
+            long l = 1;
+            while (l * l <= n)
+            {
+                if (n % l == 0)
+                {
+                    ans.Add(l);
+                    ans.Add(n / l);
+                }
+
+                l++;
+            }
+        }
+
+        HashSet<long> ans = new HashSet<long>();
+    }
+}
